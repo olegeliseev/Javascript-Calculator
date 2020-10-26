64dfa2668df5d946f09726af77570451cedb505b
Oleg Eliseev <olegeliseev96@gmail.com>
2020-10-26 18:44:07 +0300

    Added currentOperand input functionality

--- calculator.js
+++ calculator.js
@@ -1,6 +1,20 @@
-let previousOperand = document.getElementsByClassName("previous-operand");
-let currentOperand = document.getElementsByClassName("current-operand");
-let button = document.getElementsByTagName("button");
+let previousOperand = document.getElementsByClassName("previous-operand")[0];
+let currentOperand = document.getElementsByClassName("current-operand")[0];
+
+const numbers = document.getElementsByClassName("number");
+const numbersArray = Array.from(numbers);
+
+//Набор цифр в текущем операнде 
+numbersArray.forEach(num => {
+    num.addEventListener("click", () => {
+        let numValue = parseFloat(num.innerHTML);
+        currentOperand.innerHTML += numValue;
+    })
+});
+
+const operationButton = document.getElementsByClassName("operation");
+const operationArray = Array.from(operationButton);
+
 
 
 
---
+++ test.js
@@ -0,0 +1,15 @@
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+const devide = (num1, num2) => {
+    return num1 / num2;
+}
