c31732299c99044d9cb4044e33aee4c7d14fd448
Oleg Eliseev <olegeliseev96@gmail.com>
2020-10-26 15:04:27 +0300

    Basic css styling

---
+++ calculator.js
@@ -0,0 +1,7 @@
+let previousOperand = document.getElementsByClassName("previous-operand");
+let currentOperand = document.getElementsByClassName("current-operand");
+let button = document.getElementsByTagName("button");
+
+
+
+
