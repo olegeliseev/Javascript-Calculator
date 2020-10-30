d21b6bba26159d087a5c6d053dc9416300d4a568
Oleg Eliseev <olegeliseev96@gmail.com>
2020-10-30 14:39:27 +0300

    Added sum functionality

--- calculator.js
+++ calculator.js
@@ -1,21 +1,41 @@
-let previousOperand = document.getElementsByClassName("previous-operand")[0];
-let currentOperand = document.getElementsByClassName("current-operand")[0];
+const currentOperandDisplay = document.querySelector(".current-operand");
+const previousOperandDisplay = document.querySelector(".previous-operand");
+const computeButton = document.querySelector(".compute");
 
-const numbers = document.getElementsByClassName("number");
+const numbers = document.querySelectorAll(".number");
 const numbersArray = Array.from(numbers);
 
-//Набор цифр в текущем операнде 
-numbersArray.forEach(num => {
-    num.addEventListener("click", () => {
-        let numValue = parseFloat(num.innerHTML);
-        currentOperand.innerHTML += numValue;
-    })
+//Присваивает цифру нажатой кнопки к текущему операнду
+numbersArray.forEach(element => {
+    element.addEventListener("click", () => {
+        let num = element.innerHTML;
+        currentOperandDisplay.innerHTML += num;
+    });
 });
 
-const operationButton = document.getElementsByClassName("operation");
-const operationArray = Array.from(operationButton);
+const operation = () => {
+    previousOperandDisplay.innerHTML = currentOperandDisplay.innerHTML;
+    currentOperandDisplay.innerHTML = "";
+}
 
+const operationButtons = document.querySelectorAll(".operation");
+const operationButtonsArray = Array.from(operationButtons)
 
+operationButtonsArray.forEach(element => {
+    element.addEventListener("click", () => {
+        operation();
+    });
+});
 
+const compute = () => {
+    let previousOperand = parseInt(previousOperandDisplay.innerHTML);
+    let currentOperand = parseInt(currentOperandDisplay.innerHTML);
+    let result = previousOperand + currentOperand;
+    previousOperandDisplay.innerHTML = "";
+    currentOperandDisplay.innerHTML = result;
+}
 
+computeButton.addEventListener("click", () => {
+    compute();
+})
 
