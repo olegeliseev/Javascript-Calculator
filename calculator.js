4a2796cb8fce4369423883417560310c491e89d3
Oleg Eliseev <olegeliseev96@gmail.com>
2020-10-30 15:09:17 +0300

    Added all four operations functionality

--- calculator.js
+++ calculator.js
@@ -13,29 +13,45 @@ numbersArray.forEach(element => {
     });
 });
 
+//Передает значение текущего операнда в предедущий, а текущий обнуляется
 const operation = () => {
     previousOperandDisplay.innerHTML = currentOperandDisplay.innerHTML;
     currentOperandDisplay.innerHTML = "";
 }
 
 const operationButtons = document.querySelectorAll(".operation");
-const operationButtonsArray = Array.from(operationButtons)
+const operationButtonsArray = Array.from(operationButtons);
+let currentOperation; 
 
+//Присваивает переменной currentOperation знак текущей операции и вызывает функцию operation
 operationButtonsArray.forEach(element => {
     element.addEventListener("click", () => {
+        currentOperation = element.innerHTML;
         operation();
     });
 });
 
+
+//Производит операцию в зависимости от знака и обновляет значения предыдущего и текущего операндов
 const compute = () => {
     let previousOperand = parseInt(previousOperandDisplay.innerHTML);
     let currentOperand = parseInt(currentOperandDisplay.innerHTML);
-    let result = previousOperand + currentOperand;
+    let result;
+    if(currentOperation === "+") {
+        result = previousOperand + currentOperand;
+    } else if (currentOperation === "-") {
+        result = previousOperand - currentOperand;
+    } else if (currentOperation === "*") {
+        result = previousOperand * currentOperand;
+    } else if (currentOperation === "÷") {
+        result = previousOperand / currentOperand;
+    }
     previousOperandDisplay.innerHTML = "";
     currentOperandDisplay.innerHTML = result;
 }
 
+//Вызывает функцию счета при нажатии на кнопку "="
 computeButton.addEventListener("click", () => {
-    compute();
+    compute(currentOperation);
 })
 
